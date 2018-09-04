94174c2006cc3fb6a7b168d6a64eee253f40aad5
Alex <alex.bergmann87@gmail.com>
2018-09-04 20:34:19 +0200

    wine.sh

---
+++ scripts/install-wine.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#======================================================================================
+#                                
+# Author  : Alex Bergmann
+# License : Copyright (C) 2018 Alex
+#
+#======================================================================================
+
+#/etc/pacman.conf
+#[multilib] + Include uncomment
+sudo pacman -S wine
+
+sudo pacman -S wine-mono
+
+sudo pacman -S wine_gecko
