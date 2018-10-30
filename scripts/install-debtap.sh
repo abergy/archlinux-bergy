eb719d38854871febfb2fccd26a616b7a5d0a3d9
Alex <alex.bergmann87@gmail.com>
2018-10-30 07:53:13 +0100

    debtap add

---
+++ scripts/install-debtap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#======================================================================================
+#                                
+# Author  : Alex Bergmann
+# License : Copyright (C) 2018 Alex
+#
+#======================================================================================
+
+# "https://github.com/helixarch/debtap"
+
+pacaur -S --noconfirm --needed debtap
+
+sudo debtap -u
